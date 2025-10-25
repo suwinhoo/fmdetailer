9070140b7affbb0f6d6a199ed0b644ecfac19c5f
Mateusz Suwiński <mateuszsuwinski93@gmail.com>
2025-10-25 20:25:38 +0200

    +Matching players attributes to different positions

--- fmdetailer/fmdetailer/settings.py
+++ fmdetailer/fmdetailer/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # To jest WAŻNE!
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "mainpaige" / "static"
 ]
--- fmdetailer/mainpaige/urls.py
+++ fmdetailer/mainpaige/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("player_analysis/", views.player_analysis, name = "player_analysis")
+    path("", views.index, name="index"),
+    path("player_analysis/", views.player_analysis, name="player_analysis"),
+    path("add_player/", views.add_player, name="add_player"),
+    path("import_player/", views.import_player, name="import_player"),  
+    path("player/<str:player_name>/", views.player_detail, name="player_detail")
 ]
